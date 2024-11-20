d3b0fcd7036b6d872dd3bbf76c9e3e1638029ee9
AlexMAtex1992 <alessio.matessi@gmail.com>
2024-11-20 10:43:52 +0100

    Corretto

--- Esercizio1.py
+++ Esercizio1.py
@@ -1,6 +1,6 @@
 # Stampare l'equivalente di 538 minuti nel formato 12h:32min
 
-ore = 538/60
+ore = 538//60
 minuti = 538%60
 
-print ("l'equivalente hhhh di 538 minuti è {}h: {}min".format (ore, minuti))
+print ("l'equivalente di 538 minuti è {}h: {}min".format (ore, minuti))
