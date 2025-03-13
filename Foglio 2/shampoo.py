a6783579431d936cb7f6b1e10d9853edf40b69be
Alessio Matessi <alessio.matessi@gmail.com>
2025-03-13 11:25:47 +0100

    prova

--- Foglio 2/shampoo.py
+++ Foglio 2/shampoo.py
@@ -1,2 +1,15 @@
 # Scrivete una funzione sum_csv(file_name) che sommi tutti i valori delle vendite degli shampoo del file passato come argomento
 
+def sum_csv(file_name):
+    my_file = open(file_name)
+    list = []
+    line = my_file.readline ()
+    while line != '':
+        line = line. split (',')
+        list.append (line [1])
+        line = my_file.readline ()
+    my_file.close ()
+    somma = sum(list)
+    
+    return somma
+    
