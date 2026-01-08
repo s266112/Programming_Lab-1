45beef443b488a93d98e4c0793d0dc31a614fada
AlexMatex1992 <alessio.matessi@gmail.com>
2026-01-08 17:21:40 +0100

    file eliminati

--- Foglio 1/Esercizio2.py
+++
@@ -1,9 +0,0 @@
-# Definire una funzione che prende come argomento una parola e una lettera. Ritorna quante volte quella lettera è contenuta nella parola.
-
-def quante_volte (parola, lettera):
-    count = 0
-    for carattere in parola:
-        if carattere == lettera:
-            count +=1
-    return count
-print (quante_volte ("arrosto", "a"))
